3ddfb0ae75e0167587ff9affa8d1a8ebd4ef71d2
Alayn Gortazar <alayn@barnetik.com>
2014-06-24 13:40:36 +0200

    Removed remember me checkbox as it was useless

--- app/views/login/form.blade.php
+++ app/views/login/form.blade.php
@@ -15,12 +15,6 @@
                             <input class="form-control" placeholder="Password" name="password" type="password" value="">
                         </div>
 
-                        <div class="checkbox">
-                            <label>
-                                <input name="remember" type="checkbox" value="Remember Me"> Remember Me
-                            </label>
-                        </div>
-
                         <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
                     </fieldset>
                 </form>
