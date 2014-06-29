18be83f609cde5307510af778026b424ad9911ee
Alayn Gortazar <alayn@barnetik.com>
2014-06-30 00:16:49 +0200

    ../app/views/sensitiveData/index.blade.php

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,6 +1,7 @@
 <html>
     <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/bootstrap/dist/css/bootstrap-theme.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('bower_components/fontawesome/css/font-awesome.min.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('css/behigorri.css') }}" />
     <script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
