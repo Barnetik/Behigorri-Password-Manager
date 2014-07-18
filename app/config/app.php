648f5d7485a6518f26351f04ba684a7af23eb74d
Alayn Gortazar <alayn@barnetik.com>
2014-07-18 14:45:00 +0200

    app/config/app.php

--- app/config/app.php
+++ app/config/app.php
@@ -121,8 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-        'Way\Generators\GeneratorsServiceProvider',
-	),
+        'Barnetik\LaravelLdapAuth\LaravelLdapAuthServiceProvider',
+    ),
 
 	/*
 	|--------------------------------------------------------------------------
