c9625dca5e8c5d576224177041b571eebfafefbe
Alayn Gortazar <alayn@barnetik.com>
2014-07-18 13:32:50 +0200

    Moved LDAP Authentication to workbench package

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Alayn Gortazar',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'alayn@barnetik.com',
 
 );
